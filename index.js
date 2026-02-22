665bad8351c9313a78e82f3fa1bf45a1e7c8494e
unknown <mdrummanmondal2@gmail.com>
2026-02-23 03:08:08 +0600

    Successfully Created Post Method API for Challenges page

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-cluster.0hdbxiy.mongodb.net/?appName=Simple-crud-cluster`;
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+
+    const DB = client.db("ecotrack_user");
+    const myCollection = DB.collection("challenges");
+
+
+    app.post('/challenges', async(req, res)=>{
+        const newChallenge = req.body;
+        const result = await myCollection.insertOne(newChallenge)
+        res.send(result)
+
+    });
+
+    // app.get('/challenges', async(req, res)=>{
+    //     const cursor = myCollection.find();
+    //     const result = await cursor.toArray();
+    //     res.send(result)
+    // });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+} finally {
+    // Ensures that the client will close when you finish/error
+}
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
