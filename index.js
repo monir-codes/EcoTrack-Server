0b94946039cb3d2c884b3c2aedf0472d5738b1aa
unknown <mdrummanmondal2@gmail.com>
2026-04-11 14:20:50 +0600

    server connected to database

---
+++ index.js
@@ -0,0 +1,51 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 3000
+
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+// middleware
+app.use(express.json())
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@simple-crud-cluster.0hdbxiy.mongodb.net/?appName=Simple-crud-cluster`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+   const db = client.db('ecotrack_db');
+   const challengesCollection = db.collection('challenges');
+
+   app.post('/challenges', async (req, res) => {
+    const challenge = req.body;
+    const result = await challengesCollection.insertOne(challenge);
+    res.send(result);
+   })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
