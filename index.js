3ff9334a6d8dfff0eaf77731bc7ba8e3c715bc65
unknown <mdrummanmondal2@gmail.com>
2026-04-14 20:58:33 +0600

    await client.connect comment out

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const db = client.db('ecotrack_db');
     const challengesCollection = db.collection('challenges');
     const userChallengesCollection = db.collection('users_challenges');    
